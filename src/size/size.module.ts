9952f801aa6994b717557142d94ed09ff5e7c3b1
Luis Herrera <luisherrera@MacBook-Pro-de-Lucho.local>
2025-08-07 18:21:31 -0300

    up

--- src/size/size.module.ts
+++ src/size/size.module.ts
@@ -4,12 +4,14 @@ import { SizeController } from './size.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Size, SizeSchema } from './entities/size.entity';
 import { Product, ProductSchema } from '../product/entities/product.entity';
+import { Category, CategorySchema } from '../category/entities/category.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Size.name, schema: SizeSchema },
-      { name: Product.name, schema: ProductSchema }
+      { name: Product.name, schema: ProductSchema },
+      { name: Category.name, schema: CategorySchema }
     ])
   ],
   controllers: [SizeController],
